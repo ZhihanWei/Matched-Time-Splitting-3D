c9e37262b2f06e6fe4b35a4160a8f942b9ff4f30
zhihan <zhihan.wei@motionloft.com>
2018-06-15 14:11:48 -0700

    fixed stupid mistake

--- myscript.sh
+++ myscript.sh
@@ -4,6 +4,6 @@
 source /opt/asn/etc/asn-bash-profiles-special/modules.sh
 module load gcc/6.1.0
 
-mkdir build && cd build && cmake .. && make && cd ..
+mkdir result && mkdir build && cd build && cmake .. && make -j8 && cd ..
 
 ./lastpaper
